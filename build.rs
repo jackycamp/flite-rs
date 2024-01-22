f20c9641359f6871ca96e18763b6ca303c641a41
jackycamp <jackcampanella08@gmail.com>
2024-01-21 20:03:20 -0500

    debugging

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
 use std::fs;
 
+extern crate cc;
+
 fn get_c_files_in_dir_with_exclude(dir: &str, exclude: Option<&[&str]> ) -> Vec<PathBuf> {
     let mut cfiles = Vec::new();
     let dir = PathBuf::from(dir);
+
+    fs::read_dir(dir.clone()).expect(&format!("can't read dir {:?}", dir));
 
     let exclude_paths: Vec<PathBuf> = exclude
         .unwrap_or(&[])
@@ -12,6 +16,7 @@ fn get_c_files_in_dir_with_exclude(dir: &str, exclude: Option<&[&str]> ) -> Vec<
         .collect();
 
     for item in fs::read_dir(dir).unwrap() {
+        println!("item {item:?}");
         let item = item.unwrap();
         let item_path = item.path();
         if item_path.is_dir() || item_path.extension().is_none() {
@@ -46,7 +51,7 @@ fn main() {
         .files(get_c_files_in_dir("extern/flite/src/stats"))
         .files(get_c_files_in_dir("extern/flite/src/wavesynth"))
         .files(get_c_files_in_dir("extern/flite/lang/cmu_us_slt"))
-        .files(get_c_files_in_dir("extern/flite/lang/cmu_us_kal"))
+        // .files(get_c_files_in_dir("extern/flite/lang/cmu_us_kal"))
         .files(get_c_files_in_dir("extern/flite/lang/usenglish"))
         .files(get_c_files_in_dir_with_exclude("extern/flite/lang/cmulex", Some(&["cmu_lex_phones_huff_table.c", "cmu_lex_num_bytes.c", "cmu_lex_data_raw.c", "cmu_lex_entries_huff_table.c"])))
         .files(get_c_files_in_dir_with_exclude("extern/flite/src/audio", Some(&["au_oss.c", "au_alsa.c", "au_wince.c", "au_pulseaudio.c", "au_win.c", "au_palmos.c", "au_sun.c"])))
